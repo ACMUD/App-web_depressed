5be2cb4d47d8e6819c0cfabf57d79dca5969ac9f
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-07-01 12:52:05 -0500

    adicion de componentes momentaneamente necesarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NewsComponent } from './pages/news/news.component';
+import { EventosComponent } from './pages/eventos/eventos.component';
+import { GruposDeInteresComponent } from './pages/grupos-de-interes/grupos-de-interes.component';
+import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
+import { BannerComponent } from './pages/home/banner/banner.component';
+import { TopnewsComponent } from './pages/home/topnews/topnews.component';
+import { ContactoComponent } from './pages/home/contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    NewsComponent,
+    EventosComponent,
+    GruposDeInteresComponent,
+    QuienesSomosComponent,
+    BannerComponent,
+    TopnewsComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
